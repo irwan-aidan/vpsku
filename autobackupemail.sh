266964b7b09c6bc1c29e99af67f31db598981f42
irwan-aidan <91520148+irwan-aidan@users.noreply.github.com>
2021-10-09 10:39:13 +0800

    Update autobackupemail.sh

--- autobackupemail.sh
+++ autobackupemail.sh
@@ -4,8 +4,6 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-echo -e "${green}Permission Accepted...${NC}"
-fi
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
 Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
